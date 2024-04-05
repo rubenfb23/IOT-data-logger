2c564b3004ad326d926cbf9e86da3b3b62e76410
Rubén Fernández Boullón <rubenfernandezboullon@gmail.com>
2024-04-05 13:30:41 +0200

    Removed code for shutting down the system

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::fs::File;
 use std::io::Write;
-use std::process::Command;
 use sysinfo::{CpuRefreshKind, RefreshKind, System};
 fn main() {
     loop {
@@ -17,18 +16,7 @@ fn main() {
             writeln!(file, "{}: {}%", cpu.name(), cpu.cpu_usage())
                 .expect("Unable to write to file");
         }
+        
         writeln!(file, "--------------------------------------").expect("Unable to write to file");
-
-        let output = Command::new("shutdown")
-            .arg("-h")
-            .arg("now")
-            .output()
-            .expect("Failed to execute command");
-
-        writeln!(file, "status: {}", output.status).expect("Unable to write to file");
-        writeln!(file, "stdout: {}", String::from_utf8_lossy(&output.stdout))
-            .expect("Unable to write to file");
-        writeln!(file, "stderr: {}", String::from_utf8_lossy(&output.stderr))
-            .expect("Unable to write to file");
     }
 }
