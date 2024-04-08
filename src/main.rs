e1e347d36e278cac95feb7c4aa0c8a88c623918e
Rubén Fernández Boullón <rubenfernandezboullon@gmail.com>
2024-04-08 11:29:00 +0200

    Added chrono and sysinfo

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,33 @@
 use std::fs::File;
 use std::io::Write;
 use sysinfo::{CpuRefreshKind, RefreshKind, System};
+use chrono::Local;
 fn main() {
-    loop {
+
+        let mut i = 0;
+
         let mut s =
             System::new_with_specifics(RefreshKind::new().with_cpu(CpuRefreshKind::everything()));
+
+        let mut file = File::create("cpu_temp_file.txt").expect("Unable to create file");
+    
+    loop {
+
+        let time = Local::now();
 
         std::thread::sleep(std::time::Duration::from_secs(1));
 
         s.refresh_cpu();
+        
+        i+=1;
 
-        let mut file = File::create("output.txt").expect("Unable to create file");
+        writeln!(file, "------------------LINE {} ---- {} -----------------", i, time).expect("Unable to write to file");
+
+        writeln!(file, "CPU USAGE").expect("Unable to write to file");
 
         for cpu in s.cpus() {
             writeln!(file, "{}: {}%", cpu.name(), cpu.cpu_usage())
                 .expect("Unable to write to file");
-        }
+        }}
         
-        writeln!(file, "--------------------------------------").expect("Unable to write to file");
     }
-}
